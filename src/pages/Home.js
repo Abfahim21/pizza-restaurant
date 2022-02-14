7988b57b60c75b92022afc8cde5e9ec2b7c68fb0
Abfahim21 <abrarfahim21@gmail.com>
2022-02-15 04:12:38 +0600

    menu section under construction

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,8 +6,8 @@ import BannerImage from '../assets/pizza.jpeg'
 function Home() {
   return (
     <div>
-        <div className="home">
-            <div className="headerContainer" style={{backgroundImage : `url(${BannerImage})`}}>
+        <div className="home" style={{backgroundImage : `url(${BannerImage})`}}>
+            <div className="headerContainer">
                 <h1>Pizza Bangladesh</h1>
                 <p>Make Everyday Cheesy and Amazing with our Pizzas</p>
                 <Link to='menu'>
---
+++ src/pages/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Menu() {
+  return (
+    <div className='menu'>
+        <div className="menuTitle">Our Menu</div>
+    </div>
+  )
+}
+
+export default Menu
