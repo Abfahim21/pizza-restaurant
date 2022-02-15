57500c65bcebe2e13551b45708637c2b5f1ff41d
Abfahim21 <abrarfahim21@gmail.com>
2022-02-15 09:10:09 +0600

    Complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,16 +3,19 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import GithubIcon from '@mui/icons-material/GitHub'
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
         <div className="socialMedia">
-            <InstagramIcon/>
-            <FacebookIcon/>
-            <LinkedInIcon/>
-            <TwitterIcon/>
+        <a href="https://github.com/Abfahim21"><GithubIcon/></a>
+            <a href="https://facebook.com/fahmiusss"><FacebookIcon/></a>
+            <a href="https://www.linkedin.com/in/md-abrar-fahim/"><LinkedInIcon/></a>
+            <a href="https://github.com/Abfahim21"><TwitterIcon/></a>
+            <a href="https://github.com/Abfahim21"><InstagramIcon/></a>
+            
         </div>
         <p>&copy;2022 abrarfahim21@gmail.com</p>
     </div>
